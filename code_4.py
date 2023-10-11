1eec01e5659c583d270e9cea744d78d94f06f10f
pjsloat <pjsloat@miners.utep.edu>
2023-10-10 21:33:13 -0600

    updated

--- code_4.py
+++ code_4.py
@@ -2,6 +2,10 @@ import csv
 import pandas as pd
 big_mac_file = './big-mac-full-index.csv'
 df = pd.read_csv('./big-mac-full-index.csv')
+year = '2010'
+country_code = 'mys'
+dollar_price = '1.7'
+#does specifying
 
 def get_big_mac_price_by_year(year, country_code):
    query_text = f"date >= '{year}-01-01' and date <= '{year}-12-31' and iso_a3 == '{country_code.upper()}'"
@@ -29,8 +33,6 @@ def get_the_most_expensive_big_mac_price_by_year(year):
     df_result = df.query(query_text)  
     most_expensive_row = df_result.loc[df_result['dollar_price'].idxmax()]
     return f"{most_expensive_row['name']}({most_expensive_row['iso_a3']}): ${'%.1f' %most_expensive_row['dollar_price']}"
-
-    
 
 if __name__ == "__main__":  
        result_a = get_big_mac_price_by_year(2010, "kor")
